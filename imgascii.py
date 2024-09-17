129b0f222bef47dd915678be704ed84432f55c7d
d1 <devanbmohan@gmail.com>
2024-09-17 13:17:05 +0530

    code is complete

--- imgascii.py
+++ imgascii.py
@@ -10,34 +10,38 @@ def converter(image,type,saveas,scale):
    for i in range(h):
       grid.append(["X"]*w)
   	  
-  pix=img.load()
-  for y in range(h):
-    for x in range(w):
-	     if sum(pix[x,y])==0:
-	             grid[y][x]="#"
-		 elif sum(pix[x,y]) in range(1,100):
-                grid[y][x] = "X"
-            elif sum(pix[x,y]) in range(100,200):
+   pix=img.load()
+   for y in range(h):
+       for x in range(w):
+           if sum(pix[x,y])==0:
+               grid[y][x]="#"
+           elif sum(pix[x,y]) in range(1,100):
+               grid[y][x]="X"
+           elif sum(pix[x,y]) in range(100,200):
                 grid[y][x] = "%"
-            elif sum(pix[x,y]) in range(200,300):
+           elif sum(pix[x,y]) in range(200,300):
                 grid[y][x] = "&"
-            elif sum(pix[x,y]) in range(300,400):
+           elif sum(pix[x,y]) in range(300,400):
                 grid[y][x] = "*"
-            elif sum(pix[x,y]) in range(400,500):
+           elif sum(pix[x,y]) in range(400,500):
                 grid[y][x] = "+"
-            elif sum(pix[x,y]) in range(500,600):
+           elif sum(pix[x,y]) in range(500,600):
                 grid[y][x] = "/"
-            elif sum(pix[x,y]) in range(600,700):
+           elif sum(pix[x,y]) in range(600,700):
                 grid[y][x] = "("
-            elif sum(pix[x,y]) in range(700,750):
+           elif sum(pix[x,y]) in range(700,750):
                 grid[y][x] = "'"
-            else:
-                grid[y][x] = " " 
+           else:
+                grid[y][x] = " "    
+                
+                   
+                   
+ 
+ 
    art = open(saveas, "w")
-
-        for row in grid:
-             art.write("".join(row)+"\n")
+   for row in grid:
+       art.write("".join(row)+"\n")
 
    art.close()
 if __name__ == '__main__':
-    asciiConvert("mona_lisa.jpg", "jpg", "mona_lisa.txt", "3")
+    converter(" swathyuncanny.jpg", "jpg", "cityboy.txt", "3")
